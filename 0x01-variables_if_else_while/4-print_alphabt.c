f91abf8b6c2319b8f00486a3505a4240d8300101
Hoyozela <mhoyozela@gmail.com>
2022-03-12 10:05:49 -0800

    My 4th task commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,18 +10,19 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; ch++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {	
 if (alphabet == 'q')
 continue;	
 
 else if (alphabet == 'e')
+continue;
 
 putchar(alphabet);
 }
 
 putchar('\n');
 
-return 0;
+return (0);
 
 }
