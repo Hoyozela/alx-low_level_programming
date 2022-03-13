bc81567af8752248a5b82f50286ef2ed416969c6
Hoyozela <mhoyozela@gmail.com>
2022-03-12 16:03:09 -0800

    My 6th task commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+
+putchar('\n');
+
+return (0);
+
+}
