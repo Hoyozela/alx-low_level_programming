a6b54a9d99bdf015be10dfbc3477e5de8d3b7406
Hoyozela <mhoyozela@gmail.com>
2022-03-17 14:24:54 -0700

    My task 1 commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - cause an infinite loop
+ *
+ * Retur: 0
+ */
+int main(void)
+{
+int i;
+
+print("infinite loop incomming :(\n");
+
+i = 0;
+
+/*
+ *while (i < 10)
+ *putchar(i);
+}
+*/
+
+printf("infinite loop avoided! \\o/\n");
+return (0);
+}
